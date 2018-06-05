f9f5d634b19326d362bfc745e94fb86de7d06a95
qin <1602451713@qq.com>
2018-06-05 10:20:44 +0800

    user

---
+++ app/src/main/java/com/chouchongkeji/controller/user/UserController.java
@@ -0,0 +1,24 @@
+package com.chouchongkeji.controller.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author linqin
+ * @date 2018/6/5
+ */
+@RestController
+@RequestMapping("noauth/user")
+public class UserController {
+
+    /**
+     * 微信授权登录
+     * @param openId
+     * @return
+     */
+//    public Response wechatLogin(String openId){
+//
+//    }
+
+
+}
