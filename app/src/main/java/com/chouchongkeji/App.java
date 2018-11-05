60d039c2949b1d24da1981684be573db2da67eac
yichen <1119150597@qq.com>
2018-11-05 10:55:32 +0800

    app

--- app/src/main/java/com/chouchongkeji/App.java
+++ app/src/main/java/com/chouchongkeji/App.java
@@ -2,8 +2,8 @@ package com.chouchongkeji;
 
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -17,13 +17,13 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan("com.chouchongkeji.dial.dao")
 public class App extends SpringBootServletInitializer {
 //本地运行
-//    public static void main(String[] args) {
-//        SpringApplication.run(App.class, args);
-//    }
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 
 //服务器
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(App.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(App.class);
+//    }
 }
