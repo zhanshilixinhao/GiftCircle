c08498ccaecf4069d1b1fdbc8fde9ec5368e527e
lin <a00100@qq.com>
2020-01-14 11:35:19 +0800

    app

--- app/src/main/java/com/chouchongkeji/App.java
+++ app/src/main/java/com/chouchongkeji/App.java
@@ -17,7 +17,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"com.yichen.auth", "com.chouchongkeji"})
 @MapperScan("com.chouchongkeji.dial.dao")
 public class App extends SpringBootServletInitializer {
-//本
+//本地运行
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
