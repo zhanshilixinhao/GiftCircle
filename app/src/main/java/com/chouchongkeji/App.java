28b348d9b88697e81bab84660c6fa93d89bdd0c5
lin <a00100@qq.com>
2020-01-17 17:09:15 +0800

    log

--- app/src/main/java/com/chouchongkeji/App.java
+++ app/src/main/java/com/chouchongkeji/App.java
@@ -18,13 +18,13 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan("com.chouchongkeji.dial.dao")
 public class App extends SpringBootServletInitializer {
 //本地运行
-//    public static void main(String[] args) {
-//        SpringApplication.run(App.class, args);
-//    }
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 
 //服务器
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(App.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(App.class);
+//    }
 }
---
+++ app/src/main/java/com/chouchongkeji/interceptor/Log.java
@@ -0,0 +1,18 @@
+package com.chouchongkeji.interceptor;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author linqin
+ * @description log 拦截器
+ * @date 2020/1/17
+ */
+@Aspect
+@Component
+public class Log {
+
+
+
+
+}
